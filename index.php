44ba386d60e4f7f59b958f1d95121f1dc3cef11b
LeonardoSilvaOrtiz <leonardo.silvaor@correoaiep.cl>
2023-09-05 21:42:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>MENU</title>
+	</head>
+
+	<body bgcolor="ffffe0">
+	<center>
+		<H1>MENU PRINCIPAL</H1>
+			<table border=1>
+				<tr>
+					<td><a href="registroClientes.php">Registrar Clientes</a></td>
+					<td><a href="eliminarClientes2.php">Eliminar Clientes</a></td>
+					<td><a href="modificarClientes.php">Modificar Clientes</a></td>
+				</tr>
+			</table>
+			<p>Leonardo Silva O.</p>
+	</center>
+	</body>
+</html>
